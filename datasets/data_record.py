3e72aae15c7c666e897d99ed736a642e213716fb
bulatkh <bulatkhaert@gmail.com>
2022-10-17 14:45:39 +0200

    Add initial pre-processing

---
+++ datasets/data_record.py
@@ -0,0 +1,93 @@
+import numpy as np
+import os
+import pandas as pd
+
+
+class DataRecord:
+	""" Class for processing single record of sensor data
+	"""
+	def __init__(self, data_path, meta_path) -> None:
+		self.data_path = data_path
+		self.meta_path = meta_path
+
+		self.data = self.read_data()
+		self.meta = self.read_meta()
+
+		self.freq = self.extract_freq()
+
+		self.subject, self.platform, self.label, self.location, self.trial = self.extract_info()
+	
+	def read_data(self):
+		return pd.read_csv(self.data_path)
+
+	def read_meta(self):
+		return pd.read_csv(self.meta_path)
+
+	def extract_info(self):
+		filename = os.path.basename(self.data_path).split('.')[0]
+		splitted_filename = filename.split('_')
+		return splitted_filename
+	
+	def extract_freq(self):
+		return 1000 / int(self.meta.sampleRateMs[0].split('|')[0])
+
+	def get_mean_std(self):
+		"""Computes mean and std per sensor channel
+
+		Returns
+		-------
+		mean : np.ndarray
+			mean per channel
+		std : np.ndarray
+			std per channel
+		"""
+		mean = self.data.iloc[:, 2:].mean(axis=0, skipna=False)	
+		std = self.data.iloc[:, 2:].std(axis=0, skipna=False)	
+		return mean, std
+
+	def sample(self, len_tw_sec, overlap_ratio=0):
+		"""_Sample data record into shorter time-windows of the given length
+
+		Parameters
+		----------
+		len_tw_sec : int
+			length of time-windows (seconds) 
+		overlap_ratio : float
+			overlapping ratio (0 -- no overlap, 0.5 -- half overlapping time-windows)
+
+		Returns
+		-------
+		numpy.ndarray
+			array of sampled time-windows
+		"""
+		data_array = np.array(self.data.iloc[1:, 2:])
+		num_entries = int(self.freq / len_tw_sec)
+		step = num_entries * (1 - overlap_ratio)
+		assert 0 <= overlap_ratio < 1, "Overlap ratio should be in [0, 1)"
+		sampled_record = [data_array[i : i + num_entries] for i in range(0, len(data_array), step)]
+		if sampled_record[-1].shape != sampled_record[0].shape:
+			sampled_record = sampled_record[:-1]
+		assert sampled_record[0].shape[0] == num_entries, 'Record data is too small for the analysis'
+		return np.stack(sampled_record)
+
+
+def main():
+	data_path = './data/ride_data/dmitrii_android_cycle_bag_1/dmitrii_android_cycle_bag_1.csv'
+	meta_path = './data/ride_data/dmitrii_android_cycle_bag_1/meta_dmitrii_android_cycle_bag_1.csv'
+
+	data_record = DataRecord(data_path, meta_path)
+	
+	print(data_record.data.shape)
+	print(data_record.data.head())
+
+	print(data_record.extract_info())
+	print(data_record.extract_freq())
+
+	print(data_record.get_mean_std())
+
+	sampled_arr = data_record.sample(len_tw_sec=1, overlap_ratio=0)
+	print(sampled_arr.shape)
+
+
+if __name__ == '__main__':
+	main()
---
+++ preprocessing.py
@@ -0,0 +1,79 @@
+import argparse
+import os
+
+import numpy as np
+
+from datasets.data_record import DataRecord
+
+
+def parse_arguments():
+	parser = argparse.ArgumentParser()
+	
+	# data
+	parser.add_argument('--data_path', required=True)
+	parser.add_argument('--destination_path', required=True)
+
+	# splits
+	parser.add_argument('--val_sub', nargs='+', help='subjects to be used for validation')
+	parser.add_argument('--test_sub', nargs='+', required=True, help='subjects to be used for test')
+
+	# sampling
+	parser.add_argument('--len_tw_sec', type=int, default=1, help='length of time windows')
+	parser.add_argument('--overlap_ratio', type=float, default=0, help='overlapping percentage')
+
+	# normalization
+	parser.add_argument('--get_norm_vals', default=True, action='store_true', help='flag for computing mean and std based on train data')
+
+	return parser.parse_args()
+
+
+def save_sampled_record(sampled_record, record_name, destination):
+	"""Saves sampled records
+	"""
+	for i, rec in enumerate(sampled_record):
+		np.save(os.path.join(destination, f'{record_name}_{i}.npy'), rec)
+
+
+def sample_and_split(args):
+	"""Main function for sampling. Iterates through all records, sample them into short time-windows and split based on subjects; computes mean and std values on train data for further normalization.
+	"""
+	if args.get_norm_vals:
+		running_train_mean = []
+		running_train_std = []
+
+	records_dirs = [os.path.join(args.data_path, dir) for dir in os.listdir(args.data_path)]
+
+	for dir in records_dirs:
+		record_name = os.path.basename(os.path.normpath(dir))
+		data_record = DataRecord(os.path.join(dir, f'{record_name}.csv'), os.path.join(dir, f'meta_{record_name}.csv'))
+		sampled_record = data_record.sample(args.len_tw_sec, args.overlap_ratio)
+		if data_record.subject in args.test_sub:
+			save_sampled_record(sampled_record, record_name, os.path.join(args.destination_path, 'test/'))
+		elif args.val_sub is not None and data_record.subject in args.val_sub:
+			save_sampled_record(sampled_record, record_name, os.path.join(args.destination_path, 'val/'))
+		else:
+			if args.get_norm_vals:
+				cur_mean, cur_std = data_record.get_mean_std()
+				running_train_mean.append(cur_mean)
+				running_train_std.append(cur_std)
+			save_sampled_record(sampled_record, record_name, os.path.join(args.destination_path, 'train/'))
+
+	if args.get_norm_vals:
+		ovr_mean = np.stack(running_train_mean).mean(axis=0)
+		ovr_std = np.stack(running_train_std).mean(axis=0)
+		
+		np.save(os.path.join(args.destination_path, 'ovr_mean.npy'), ovr_mean)
+		np.save(os.path.join(args.destination_path, 'ovr_std.npy'), ovr_std)
+	
+
+def main():
+	args = parse_arguments()
+	os.makedirs(args.destination_path, exist_ok=True)
+	os.makedirs(os.path.join(args.destination_path, 'train'), exist_ok=True)
+	os.makedirs(os.path.join(args.destination_path, 'val'), exist_ok=True)
+	os.makedirs(os.path.join(args.destination_path, 'test'), exist_ok=True)
+	sample_and_split(args)
+
+
+if __name__ == '__main__':
+	main()
